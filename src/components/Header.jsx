99c6d80f6c632f626e574fc0444ed19ad0f8ec6c
mustaavaruus <seppenen.i@yandex.ru>
2021-05-16 19:43:01 +0300

    very first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://cdn.freelogovectors.net/wp-content/uploads/2016/12/nasa-logo.png" />
+        </header>);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://static01.nyt.com/images/2018/11/30/science/30nasa-01/30nasa-01-superJumbo.jpg" />
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                my posts
+          <div>
+                    new post
+          </div>
+                <div>
+                    <div>
+                        post 1
+              </div>
+                    <div>
+                        post 2
+              </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Profile;
